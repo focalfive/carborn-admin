9499e95a195a5bf7c771d62b6664e1a5055ca4fd
pureye4u <pureye4u@gmail.com>
2018-04-09 14:51:10 +0900

    [build] build script

---
+++ src/build_dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Variables
+PROJECT_NAME=carborn-admin
+SOURCE_DIR=/usr/share/nginx/html
+TARGET=develop
+TARGET_PORT=4200
+IMAGE_NAME=${PROJECT_NAME}:${TARGET}
+
+echo "TARGET: ${TARGET}"
+echo "CONTAINER: ${PROJECT_NAME}"
+echo "IMAGE: ${IMAGE_NAME}"
+
+git checkout -b ${TARGET}
+
+git pull
+
+docker stop ${PROJECT_NAME}
+
+docker rm ${PROJECT_NAME}
+
+docker rmi ${IMAGE_NAME}
+
+docker build -t ${IMAGE_NAME} .
+
+docker run --name ${PROJECT_NAME} -d -p ${TARGET_PORT}:80 ${IMAGE_NAME}
