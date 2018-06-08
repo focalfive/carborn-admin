6949635fd0a5939975642ba33a8dd902caab061c
pureye4u <pureye4u@gmail.com>
2018-06-09 01:41:50 +0900

    Replace port

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -2310,7 +2310,7 @@ var environment = {
     mLabApiAdminObjectId: '5a168f0b734d1d3ed22d2509',
     googleClientId: '598923188772-mnusf73277ui7pq88o4lh2tpqkhml460.apps.googleusercontent.com',
     googleClientSecret: 'LeJR4jQbfvsazbFYk5L_-1Ui',
-    googleRedirectUri: 'http://slows.me:8080/login',
+    googleRedirectUri: 'http://slows.me:4200/login',
 };
 
 
